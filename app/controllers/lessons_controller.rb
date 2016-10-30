4f3439831ff952fdc3d7c3a2aff17a8b11bd529c
Jack Tang <jack.t@outlook.com>
2016-10-30 05:11:12 +0000

    permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_course
+
   def show
   end
 
@@ -6,5 +9,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrollment_for_course
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled in this course'
+    end
   end
 end
